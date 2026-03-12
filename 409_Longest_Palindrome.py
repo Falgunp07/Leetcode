a51e68054c71aa0e50fad23d9de49271d1326232
Falgun <falgunpatel071@gmail.com>
2026-03-12 22:34:29 +0530

    Add solution for 409: Longest Palindrome

---
+++ 409_Longest_Palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq = Counter(s)
+        length = 0
+        odd = False
+
+        for count in freq.values():
+            if count % 2 == 0:
+                length += count
+            else:
+                length += count - 1
+                odd = True
+
+        if odd:
+            length += 1
+
+        return length
