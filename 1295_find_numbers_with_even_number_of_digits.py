baf559117d5b208402dd93579c0e07c91793b1e3
Falgun <falgunpatel071@gmail.com>
2026-03-17 18:08:20 +0530

    Add solution for LeetCode 1295

---
+++ 1295_find_numbers_with_even_number_of_digits.py
@@ -0,0 +1,14 @@
+from typing import List
+import math
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        even_count = 0
+        
+        for num in nums:
+            digit_count = int(math.log10(num)) + 1
+            
+            if digit_count % 2 == 0:
+                even_count += 1
+                
+        return even_count
