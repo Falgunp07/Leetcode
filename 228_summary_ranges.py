5c2e4f6371ccfce46b1b34e86963a4db71ee55ca
Falgun <falgunpatel071@gmail.com>
2026-04-06 21:34:59 +0530

    Add 228_summary_ranges solution

---
+++ 228_summary_ranges.py
@@ -0,0 +1,18 @@
+class Solution:
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        ans = []
+        i = 0
+        n = len(nums)
+        
+        while i < n:
+            start = nums[i]
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+            if start == nums[i]:
+                ans.append(str(start))
+            else:
+                ans.append(str(start) + "->" + str(nums[i]))
+            
+            i = i + 1
+            
+        return ans
